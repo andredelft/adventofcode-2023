d878ef9dbe3c9df52ce160dbf5463dbee40391b8
André van Delft <andrevandelft@outlook.com>
2023-12-15 19:42:24 +0100

    Small cleanup

--- day_14/parabolic_reflector_dish.py
+++ day_14/parabolic_reflector_dish.py
@@ -1,5 +1,4 @@
 from tqdm import tqdm
-from functools import cache
 from itertools import count
 
 from lib.field import Field
@@ -21,8 +20,7 @@ def solve_a(input_string: str):
     return load
 
 
-def cycle(field: Field):
-    field = field.copy()
+def tilt_and_rotate(field: Field):
     for _ in range(4):
         # Tilt
         current_roll_position = [0 for _ in range(field.width)]
@@ -38,7 +36,7 @@ def cycle(field: Field):
         # Rotate
         field = Field(
             [
-                reversed([field[i, j] for i in range(field.width)])
+                [field[(field.height - 1) - i, j] for i in range(field.width)]
                 for j in range(field.height)
             ]
         )
@@ -58,15 +56,19 @@ def solve_b(input_string: str, N=1_000_000_000):
     field = Field(input_string)
     history: dict[Field, int] = dict()
 
-    for n in tqdm(count(start=1), desc="Tilting and shifting"):
-        field = cycle(field)
+    for n in tqdm(count(start=1), desc="Tilting and rotating"):
+        # We build the new field from a copy to prevent mutations in the history
+        # dictionary keys.
+        field = tilt_and_rotate(field.copy())
 
         if field in history.keys():
             break
         else:
             history[field] = n
 
-    #
+    # When the current state is already encountered in the history, the future
+    # will become cyclical. We calculate the period of these cycles and extrapolate
+    # to determine what the final state of the field will be after N steps.
     n_1 = history[field]
     n_2 = n
 
